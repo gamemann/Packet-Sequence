dd93ee3ed774e9057cebb0a6ef00878f84c0dd71
Christian Deacon <gamemann@gflclan.com>
2020-10-23 17:00:11 +0000

    Add main program files and checkpoint.

---
+++ src/pcktseq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "pcktseq.h"
+#include "config.h"
+#include "cmdline.h"
+#include "utils.h"
+
+int main()
+{
+
+}
---
+++ src/pcktseq.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define MAXSEQUENCES 256
