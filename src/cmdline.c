23147b145ca647badcbd4622dae5a81dbb832606
Christian Deacon <gamemann@gflclan.com>
2020-10-23 17:09:51 +0000

    Add comment to parsecmdline().

--- src/cmdline.c
+++ src/cmdline.c
@@ -2,7 +2,15 @@
 
 #include "cmdline.h"
 
+/**
+ * Parses the command line options using getopt.
+ * 
+ * @param argv The argument array pointer passed in the `int main()` function.
+ * @param argc The argument counter passed in the `int main()` function.
+ * @param cmd A pointer to the `cmdline` structure that stores all command line values.
+ * @return void
+ */
 void parsecmdline(char *argv[], int argc, struct cmdline *cmd)
 {
-    
+
 }
