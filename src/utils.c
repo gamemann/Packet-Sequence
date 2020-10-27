b1a17bc17bd272a10b70cbc050bcebfcf49245fa
Christian Deacon <gamemann@gflclan.com>
2020-10-27 12:33:22 +0000

    Add function to pick a random IP from a given CIDR range.

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <ctype.h>
+#include <time.h>
+#include <string.h>
+
+#include <arpa/inet.h>
 
 /**
  * Retrieves the Ethernet MAC of the host's default gateway and stores it in `mac` (uint8_t *).
@@ -56,4 +60,72 @@ char *lowerstr(char *str)
     }
 
     return str;
+}
+
+/**
+ * Chooses a random IP from a specific CIDR range.
+ * 
+ * @param range The range in IP/CIDR format.
+ * @return The pointer to a string with the random IP within the CIDR range.
+ * @note Thanks for the help on https://stackoverflow.com/questions/64542446/choosing-a-random-ip-from-any-specific-cidr-range-in-c.
+ */
+char *randomip(char *range, uint64_t *pcktcount)
+{
+    // Split the <ip>/<cidr> and assign both values.
+    char *split;
+
+    char *sip = NULL;    
+    char *cidrstr = NULL;
+
+    char *str = malloc(sizeof(char) * (strlen(range) + 1));
+    strcpy(str, range);
+
+    split = strtok(str, "/");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (split == NULL)
+        {
+            break;
+        }
+
+        if (i == 0)
+        {
+            sip = strdup(split);
+        }
+        else
+        {
+            cidrstr = strdup(split);
+        }
+        
+        split = strtok(NULL, "/");
+    }
+
+    // Free the temporary string (str).
+    free(str);
+
+    uint8_t cidr = (uint8_t) atoi(cidrstr);
+
+    // Randomize the rand_r() seed.
+    unsigned int seed = time(NULL) + (unsigned long)*pcktcount;
+
+    // Create in_addr and convert the IP string to a 32-bit integer.
+    struct in_addr inaddr;
+    inet_aton(sip, &inaddr);
+    uint32_t ipaddr = ntohl(inaddr.s_addr);
+
+    // Get the mask (the complement of 2 to the power of the CIDR minus one).
+    uint32_t mask = (1 << (32 - cidr)) - 1;
+
+    // Generate a random number using rand_r(&seed).
+    uint32_t randnum = rand_r(&seed);
+
+    // Attempt to pick a random IP from the CIDR range. We shift left by the CIDR range since it's big endian. 
+    uint32_t randip = (ipaddr & ~mask) | (mask & randnum);
+
+    // Convert the new IP to a string and print it.
+    struct in_addr randipstr;
+    randipstr.s_addr = htonl(randip);
+
+    return inet_ntoa(randipstr);
 }
--- src/utils.h
+++ src/utils.h
@@ -5,3 +5,4 @@
 void getgwmac(uint8_t *mac);
 uint16_t randnum(uint16_t min, uint16_t max, unsigned int seed);
 char *lowerstr(char *str);
+char *randomip(char *range, uint64_t *pcktcount);
